297dc97d02c2bfb43e1de7dd74e662eaa3952176
Andrea Corredor <andie@Andreas-MacBook-Pro.local>
2023-03-30 19:04:44 +0200

    Use gin validation for query params
    * Move validation of query params to gin's validator.
    * Set up logger with configurable level.
    * Add tests.

--- main.go
+++ main.go
@@ -1,100 +1,95 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "strconv"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
-// TODO error wrapping
-// TODO logging
-// TODO tests
-// TODO make port configurable via env variable
-
-const (
-  LimitDefault = "100"
-  Int1Default = "3"
-  Int2Default = "5"
-  Str1Default = "fizz"
-  Str2Default = "buzz"
-)
+// TODO consider refactoring into models, handlers, fizzbuzz and move tests too
 
 type FizzBuzzConfig struct {
-  Int1, Int2, Limit int
-  Str1, Str2 string
+	Int1  int    `form:"int1" binding:"required,numeric,max=2147483647,min=-2147483648"`
+	Int2  int    `form:"int2" binding:"required,numeric,max=2147483647,min=-2147483648"`
+	Limit int    `form:"limit" binding:"required,numeric,max=2147483647,min=1"`
+	Str1  string `form:"str1" binding:"required"`
+	Str2  string `form:"str2" binding:"required"`
 }
 
 type FizzBuzzResult struct {
 	Result []string `json:"result"`
 }
 
-
 func fizzbuzz(config FizzBuzzConfig) FizzBuzzResult {
-  result := make([]string, 0)
+	result := make([]string, 0)
 
-  for i := 1; i <= config.Limit; i++ {
-    if i % (config.Int1 * config.Int2) == 0 {
-      result = append(result, config.Str1 + config.Str2)
-    } else if i % config.Int1 == 0 {
-      result = append(result, config.Str1)
-    } else if i % config.Int2 == 0 {
-      result = append(result, config.Str2)
-    } else {
-      result = append(result, fmt.Sprint(i))
-    }
-  }
+	log.WithFields(
+		log.Fields{"str1": config.Str1, "str2": config.Str2, "limit": config.Limit, "int1": config.Int1, "int2": config.Int2},
+	).Debug("Computing FizzBuzz")
+	for i := 1; i <= config.Limit; i++ {
+		if i%config.Int1 == 0 && i%config.Int2 == 0 {
+			result = append(result, fmt.Sprintf("%s%s", config.Str1, config.Str2))
+		} else if i%config.Int1 == 0 {
+			result = append(result, config.Str1)
+		} else if i%config.Int2 == 0 {
+			result = append(result, config.Str2)
+		} else {
+			result = append(result, fmt.Sprint(i))
+		}
+	}
 
-  return FizzBuzzResult{Result: result}
-}
-
-func parseAndValidateQueryParams(ctx *gin.Context) (*FizzBuzzConfig, error) {
-  limitStr := ctx.DefaultQuery("limit", LimitDefault)
-  int1Str := ctx.DefaultQuery("int1", Int1Default)
-  int2Str := ctx.DefaultQuery("int2", Int2Default)
-  str1 := ctx.DefaultQuery("str1", Str1Default)
-  str2 := ctx.DefaultQuery("str2", Str2Default)
-
-  limit, err := strconv.Atoi(limitStr)
-  if err != nil {
-    return nil, err
-  }
-
-  int1, err := strconv.Atoi(int1Str)
-  if err != nil {
-    return nil, err
-  }
-
-  int2, err := strconv.Atoi(int2Str)
-  if err != nil {
-    return nil, err
-  }
-
-  config := FizzBuzzConfig{
-    Limit: limit,
-    Int1: int1,
-    Int2: int2,
-    Str1: str1,
-    Str2: str2,
-  }
-
-  return &config, nil
+	return FizzBuzzResult{Result: result}
 }
 
 func fizzbuzzHandler(ctx *gin.Context) {
-  config, err := parseAndValidateQueryParams(ctx)
-  if err != nil {
-    // TODO return an appropriate HTTP error
-  }
+	config := FizzBuzzConfig{}
+	if err := ctx.ShouldBind(&config); err != nil {
+		log.Error("Error parsing query params: ", err)
+		// TODO consider improving these error messages
+		ctx.JSON(http.StatusBadRequest, gin.H{"errors": strings.Split(err.Error(), "\n")})
+		return
+	}
 
-  ctx.JSON(http.StatusOK, fizzbuzz(*config))
+	ctx.JSON(http.StatusOK, fizzbuzz(config))
+}
+
+func setUpRouter() *gin.Engine {
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	// Turn panics into 500s
+	router.Use(gin.Recovery())
+
+	router.GET("/api/v1/fizzbuzz", fizzbuzzHandler)
+
+	return router
+}
+
+func setUpLogger() {
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+
+	levelStr, ok := os.LookupEnv("LOG_LEVEL")
+	if !ok {
+		levelStr = "INFO"
+	}
+
+	logLevel, err := log.ParseLevel(levelStr)
+	if err != nil {
+		logLevel = log.InfoLevel
+	}
+
+	log.SetLevel(logLevel)
+}
+
+func init() {
+	setUpLogger()
 }
 
 func main() {
-  router := gin.New()
-
-  router.GET("/api/v1/fizzbuzz", fizzbuzzHandler)
-
-  router.Run()
+	router := setUpRouter()
+	router.Run()
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,92 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFizzBuzz(t *testing.T) {
+	cases := []struct {
+		config   FizzBuzzConfig
+		expected []string
+	}{
+		{
+			FizzBuzzConfig{Int1: 2, Int2: 3, Limit: 10, Str1: "fizz", Str2: "buzz"},
+			[]string{"1", "fizz", "buzz", "fizz", "5", "fizzbuzz", "7", "fizz", "buzz", "fizz"},
+		},
+		{
+			FizzBuzzConfig{Int1: 2, Int2: 3, Limit: 0, Str1: "fizz", Str2: "buzz"},
+			[]string{},
+		},
+		{
+			FizzBuzzConfig{Int1: -2, Int2: 3, Limit: 6, Str1: "abc", Str2: "123"},
+			[]string{"1", "abc", "123", "abc", "5", "abc123"},
+		},
+		{
+			FizzBuzzConfig{Int1: 2, Int2: 2, Limit: 10, Str1: "foo", Str2: "bar"},
+			[]string{"1", "foobar", "3", "foobar", "5", "foobar", "7", "foobar", "9", "foobar"},
+		},
+	}
+
+	for _, testCase := range cases {
+		actual := fizzbuzz(testCase.config).Result
+		assert.Equal(t, testCase.expected, actual)
+	}
+}
+
+func TestGetFizzBuzzHTTP_Success(t *testing.T) {
+	router := setUpRouter()
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/fizzbuzz?str1=foo&str2=bar&limit=25&int1=7&int2=3", nil)
+	router.ServeHTTP(resp, req)
+	expectedBody := `{"result":["1","2","bar","4","5","bar","foo","8","bar","10","11","bar","13","foo","bar","16","17","bar","19","20","foobar","22","23","bar","25"]}`
+
+	assert.Equal(t, http.StatusOK, resp.Code)
+	assert.Equal(t, resp.Header().Get("Content-Type"), "application/json; charset=utf-8")
+	assert.Equal(t, expectedBody, resp.Body.String())
+}
+
+func TestQueryParamValidation(t *testing.T) {
+	router := setUpRouter()
+
+	cases := []struct {
+		queryParams   string
+		expectedError string
+	}{
+		// Required
+		{"str2=bar&limit=25&int1=7&int2=3", "Error:Field validation for 'Str1' failed on the 'required' tag"},
+		{"str1=bar&limit=25&int1=7&int2=3", "Error:Field validation for 'Str2' failed on the 'required' tag"},
+		{"str1=bar&str2=foo&int1=7&int2=3", "Error:Field validation for 'Limit' failed on the 'required' tag"},
+		{"str1=bar&str2=foo&limit=100&int2=3", "Error:Field validation for 'Int1' failed on the 'required' tag"},
+		{"str1=bar&str2=foo&limit=100&int1=7", "Error:Field validation for 'Int2' failed on the 'required' tag"},
+
+		// Max values
+		{"str1=bar&str2=foo&limit=2147483657&int1=7&int2=3", "Error:Field validation for 'Limit' failed on the 'max' tag"},
+		{"str1=bar&str2=foo&limit=100&int1=2147483657&int2=3", "Error:Field validation for 'Int1' failed on the 'max' tag"},
+		{"str1=bar&str2=foo&limit=100&int1=7&int2=2147483657", "Error:Field validation for 'Int2' failed on the 'max' tag"},
+
+		// Min value
+		{"str1=bar&str2=foo&limit=-12&int1=7&int2=3", "Error:Field validation for 'Limit' failed on the 'min' tag"},
+		{"str1=bar&str2=foo&limit=100&int1=-2147483657&int2=3", "Error:Field validation for 'Int1' failed on the 'min' tag"},
+		{"str1=bar&str2=foo&limit=100&int1=7&int2=-2147483657", "Error:Field validation for 'Int2' failed on the 'min' tag"},
+
+		// Numeric
+		{"str1=bar&str2=foo&limit=ABC&int1=7&int2=3", "strconv.ParseInt: parsing \\\"ABC\\\": invalid syntax"},
+		{"str1=bar&str2=foo&limit=100&int1=FOO&int2=3", "strconv.ParseInt: parsing \\\"FOO\\\": invalid syntax"},
+		{"str1=bar&str2=foo&limit=100&int1=7&int2=BAR", "strconv.ParseInt: parsing \\\"BAR\\\": invalid syntax"},
+	}
+
+	for _, testCase := range cases {
+		resp := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", fmt.Sprintf("%s%s", "/api/v1/fizzbuzz?", testCase.queryParams), nil)
+		router.ServeHTTP(resp, req)
+
+		assert.Equal(t, http.StatusBadRequest, resp.Code)
+		assert.Equal(t, resp.Header().Get("Content-Type"), "application/json; charset=utf-8")
+		assert.Contains(t, resp.Body.String(), testCase.expectedError)
+	}
+}
